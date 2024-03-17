a3f6a4d1334fe83707f4a17718251fa03f7cc507
IsaiahSama <zeldaplayergl@gmail.com>
2024-03-17 10:49:07 -0400

    Fixed up thedefault styling

--- Isaiah_Portfolio/src/main.tsx
+++ Isaiah_Portfolio/src/main.tsx
@@ -10,16 +10,19 @@ import About from './pages/About'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
 import Loading from './pages/Loading'
+import Layout from './components/Layout'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Loading />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
+        <Route path="/*" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="skills" element={<Skills />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
